2b4bfffd6e0c4fc423951da9a9391a50ed35eb10
ashton14 <ashtonalonge14@gmail.com>
2025-06-04 20:26:17 -0700

    removed unnecessary code

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -10,27 +10,7 @@ export default function RestaurantDetails({ restaurants }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const pageRef = useRef(null);
-  const [showSignIn, setShowSignIn] = useState(false);
-  const [showSignUp, setShowSignUp] = useState(false);
-
-
-  const handleCloseSignIn = () => {
-    setShowSignIn(false);
-  };
-
-  const handleCloseSignUp = () => {
-    setShowSignUp(false);
-  };
-
-  const handleSwitchToSignUp = () => {
-    setShowSignIn(false);
-    setShowSignUp(true);
-  };
-
-  const handleSwitchToSignIn = () => {
-    setShowSignUp(false);
-    setShowSignIn(true);
-  };
+  
   
   const restaurant = restaurants?.find(r => r.id === parseInt(id));
   console.log('Found restaurant:', restaurant);
